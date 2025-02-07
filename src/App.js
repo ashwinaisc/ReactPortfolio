d926412b85bc2e67d8e8736b0d63421bc45adf89
ashwinaisc <aisthedesigner@gmail.com>
2025-02-07 05:14:15 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import Banner from './components/Banner';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Services from './components/Services';
+import Work from './components/Work';
+function App(){
+  return(
+  <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+
+    <Nav />
+    <About />
+
+  </div>
   );
+
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+const Nav = () =>{
+    return (
+    <nav>
+        <a href="/">Portfolio</a>
+        <div>
+            <ul>
+                <li>
+                <a href="#About">About</a>
+                </li>
+                <li>
+                <a href="#Services">Services</a>
+                </li>
+                <li>
+                <a href="#Work">Work</a>
+                </li>
+                <li>
+                <a href="#Contact">Contact</a>
+                </li>
+       
+
+            </ul>
+        </div>
+    </nav>
+    )
+}
+
+export default Nav;
